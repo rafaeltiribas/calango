29aa4088c73a4be0f6a1f2c54a44bb5c23db7e1a
Rafael Tiribás <rafaeltiribas@outlook.com>
2024-08-14 19:10:44 -0300

    first version

---
+++ backend/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+	"time"
+)
+
+func connectToMongoDB() (*mongo.Client, context.Context, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client, ctx, nil
+}
+
+func main() {
+	client, ctx, err := connectToMongoDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	collection := client.Database("calango").Collection("url")
+
+	r := gin.Default()
+
+	r.POST("/calango", func(c *gin.Context) {
+		longUrl := c.PostForm("url")
+
+		reqCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		shortUrl := urlShortener(longUrl, collection, reqCtx)
+
+		c.JSON(http.StatusOK, gin.H{
+			"shortUrl": shortUrl,
+		})
+	})
+
+	r.GET("/:url", func(c *gin.Context) {
+		shortUrl := c.Param("url")
+
+		reqCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		var result struct {
+			LongUrl string `bson:"longUrl"`
+		}
+		err := collection.FindOne(reqCtx, bson.M{"_id": shortUrl}).Decode(&result)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		c.Redirect(http.StatusMovedPermanently, result.LongUrl)
+	})
+
+	r.Run()
+}
---
+++ backend/url_shortener.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+)
+
+func urlShortener(longUrl string, collection *mongo.Collection, ctx context.Context) string {
+
+	urlencoder := sha256.New()
+	urlencoder.Write([]byte(longUrl))
+	encodedurl := urlencoder.Sum(nil)
+	encodedStr := hex.EncodeToString(encodedurl)
+
+	shorturl := encodedStr[:5]
+
+	_, err := collection.InsertOne(ctx, bson.M{
+		"_id":     shorturl,
+		"longUrl": longUrl,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return shorturl
+}
