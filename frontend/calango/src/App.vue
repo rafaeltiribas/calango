6765029c24558f57592c3a7d753e27acc1ca6d8e
Rafael Tiribás <rafaeltiribas@outlook.com>
2024-08-15 10:03:54 -0300

    feat frontend landing page

---
+++ frontend/calango/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Calango from './components/Calango.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://github.com/rafaeltiribas/calango" target="_blank">
+      <img src="./assets/calango.png" class="logo" alt="Calango logo" />
+    </a>
+  </div>
+  <Calango msg="Calango" />
+</template>
+
+<style scoped>
+.logo {
+  height: 8em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em green);
+}
+</style>
---
+++ frontend/calango/src/components/Calango.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p class="read-the-docs">Insert the URL that you want to be shortened</p>
+
+  <div class="card">
+
+  </div>
+
+  <p>
+    Check out
+    <a href="https://www.linkedin.com/in/rafaeltiribas/" target="_blank"
+      >my LinkedIn</a
+    > to see more projects I'm working on
+  </p>
+  <p class="read-the-docs">Click on the Calango logo to go to project's Github repository</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
